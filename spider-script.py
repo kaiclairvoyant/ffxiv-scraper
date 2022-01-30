cf2be669ec16a92645540541d4fddd5849e65253
kaiclairvoyant <>
2022-01-30 20:18:51 -0300

    updating the script to do everything in one command

--- spider-script.py
+++ spider-script.py
@@ -6,6 +6,11 @@ startTime = datetime.now()
 jobs = ['reaper', 'sage', 'summoner', 'paladin', 'astrologian', 'warrior', 'gunbreaker', 'darkknight', 'dragoon', 'whitemage', 'blackmage', 'dancer', 'machinist', 'bard', 'redmage', 'ninja', 'monk', 'samurai', 'scholar']
 
 for job in jobs:
-    os.system("scrapy crawl ffxiv_spider -a job=% s" % job)
+    print("Running ffxiv_spider for % s" % job)
+    os.system("scrapy crawl ffxiv_spider -a job=% s" % job + " -s LOG_ENABLED=False")
 
-print("Finished in: " + str((datetime.now() - startTime).total_seconds()) + " seconds")
+print("Downloads were completed in " + str((datetime.now() - startTime).total_seconds()) + " seconds")
+
+print("Building data with dataBuilder.py")
+
+os.system("py dataBuilder.py")
